387e7a57ce1c3c03f8ccff879b4bdfa566bc4f8e
Justas Puodžius <justas.puodzius@gmail.com>
2019-03-15 18:39:51 +0200

    Exception Handling
    Added exception handling when Lamp is unresponsive/.

--- LampControl/LampWebCommands.cs
+++ LampControl/LampWebCommands.cs
@@ -11,20 +11,44 @@ namespace LampControl
     {
         public static void LightOn()
         {
-            HttpWebRequest lightOn = (HttpWebRequest)WebRequest.Create("http://192.168.0.22/lightsOFF");
-            lightOn.Timeout = 10000;
-            HttpWebResponse lightOnResp = (HttpWebResponse)lightOn.GetResponse();
-            lightOn.Abort();
-            if (lightOnResp.StatusCode.ToString() == "OK") { new LogWriter("LightOn "); }
+            try
+            {
+                HttpWebRequest lightOn = (HttpWebRequest) WebRequest.Create("http://192.168.0.22/lightsOFF");
+                lightOn.Timeout = 10000;
+                HttpWebResponse lightOnResp = (HttpWebResponse) lightOn.GetResponse();
+                lightOn.Abort();
+                if (lightOnResp.StatusCode.ToString() == "OK")
+                {
+                    new LogWriter("LightOn ");
+                }
+            }
+            catch(Exception e)
+            {
+                new LogWriter("Lamp Unresponsive");
+                throw;
+            }
+
+            
         }
 
         public static void LightOff()
         {
-            HttpWebRequest lightOff = (HttpWebRequest)WebRequest.Create("http://192.168.0.22/lightsON");
-            lightOff.Timeout = 10000;
-            HttpWebResponse lightOffResp = (HttpWebResponse)lightOff.GetResponse();
-            lightOff.Abort();
-            if (lightOffResp.StatusCode.ToString() == "OK") { new LogWriter("LightOff"); }
+            try
+            {
+                HttpWebRequest lightOff = (HttpWebRequest) WebRequest.Create("http://192.168.0.22/lightsON");
+                lightOff.Timeout = 4000;
+                HttpWebResponse lightOffResp = (HttpWebResponse) lightOff.GetResponse();
+                lightOff.Abort();
+                if (lightOffResp.StatusCode.ToString() == "OK")
+                {
+                    new LogWriter("LightOff");
+                }
+            }
+            catch (Exception e)
+            {
+                new LogWriter("Lamp Unresponsive");
+                throw;
+            }
         }
     }
 }
